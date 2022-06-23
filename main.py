39df3c915d6e9c05b12a7d42e579c7dec3af72fc
Anton <kotbond437@gmail.com>
2022-06-23 20:59:03 +0300

    add func "isCollision" realization implementation new alien

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import math
+
 import pygame
 import random
 
@@ -25,7 +27,7 @@ playerX_change = 0
 
 #  Alien
 alienImg = pygame.image.load(random.choice(['alien/alien.png', 'alien/space-ship.png', 'alien/alien2.png']))
-alienX = random.randint(0, 800)
+alienX = random.randint(0, 735)
 alienY = random.randint(50, 50)
 alienX_change = 0.3
 alienY_change = 20
@@ -38,6 +40,8 @@ bulletY = 480
 bulletX_change = 0
 bulletY_change = 1
 bullet_state = 'ready'
+
+score = 0
 
 
 def player(x, y):
@@ -52,6 +56,14 @@ def fire_bullet(x, y):
     global bullet_state
     bullet_state = 'fire'
     screen.blit(bulletImg, (x + 16, y + 10))
+
+
+def isCollision(alienX, alienY, bulletX, bulletY):
+    distance = math.sqrt((math.pow(alienX - bulletX, 2)) + (math.pow(alienY - bulletY, 2)))
+    if distance < 27:
+        return True
+    else:
+        return False
 
 
 #  Game Loop
@@ -70,9 +82,9 @@ while running:
     #  if keystroke is pressed check whether its right or left
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_LEFT:
-                playerX_change = -0.4
+                playerX_change = -0.5
             if event.key == pygame.K_RIGHT:
-                playerX_change = 0.4
+                playerX_change = 0.5
             elif event.key == pygame.K_SPACE:
                 if bullet_state == 'ready':
                     # Get the current x coordinate of te spaceship
@@ -108,6 +120,17 @@ while running:
     if bullet_state == 'fire':
         fire_bullet(bulletX, bulletY)
         bulletY -= bulletY_change
+
+    # Collision
+    collision = isCollision(alienX, alienY, bulletX, bulletY)
+    if collision:
+        bulletY = 480
+        bullet_state = 'ready'
+        score += 1
+        print(score)
+        alienImg = pygame.image.load(random.choice(['alien/alien.png', 'alien/space-ship.png', 'alien/alien2.png']))
+        alienX = random.randint(0, 735)
+        alienY = random.randint(50, 50)
 
     player(playerX, playerY)
     alien(alienX, alienY)
